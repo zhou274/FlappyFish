1bf86d0133c090ae0971c4637fb828910b6f5b08
zhou274 <274891948@qq.com>
2024-12-22 17:32:14 +0800

    test

---
+++ FlappyFishGameAndroid-master/Assets/Score.cs
@@ -0,0 +1,73 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public static int score = 0;
+	public static int highScore = 0;
+	static bool isContinue=false;
+	public TextMeshProUGUI GameOverScoreText;
+    public TextMeshProUGUI GameOverHighScoreText;
+
+    static Score instance;
+
+	static public void AddPoint() {
+		if(instance.fish.dead)
+			return;
+
+		score++;
+
+		//if(score > highScore) {
+		//	highScore = score;
+		//}
+	}
+
+	Fish fish;
+
+	void Start() {
+		instance = this;
+		GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+		if(player_go == null) {
+			Debug.LogError("Could not find an object with tag 'Player'.");
+		}
+
+		fish = player_go.GetComponent<Fish>();
+		if(isContinue==false)
+		{
+            score = 0;
+        }
+		else if (isContinue == true)
+		{
+			isContinue = false;
+		}
+		highScore = PlayerPrefs.GetInt("highScore", 0);
+	}
+
+	void OnDestroy() {
+		instance = null;
+		PlayerPrefs.SetInt("highScore", highScore);
+	}
+
+	void Update () {
+
+		transform.GetComponentInChildren<Text>().text = "当前得分: " + score;//+ "\n最高分: " + highScore;
+		GameOverScoreText.text = score.ToString();
+		GameOverHighScoreText.text = highScore.ToString();
+
+
+	}
+	public void Continue()
+	{
+		isContinue = true;
+		SceneManager.LoadScene(0);
+	}
+	public void ReLoad()
+	{
+        SceneManager.LoadScene(0);
+    }
+}
+
+
---
+++ FlappyFishGameAndroid-master/Assets/StartScreenScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+
+	static bool sawOnce = false;
+	public GameObject startText;
+
+	// Use this for initialization
+	void Start () {
+		if(!sawOnce) {
+			startText.SetActive(true);
+			//GetComponent<SpriteRenderer>().enabled = true;
+			Time.timeScale = 0;
+		}
+
+		sawOnce = true;
+	}
+	
+	//Update is called once per frame
+	void Update () {
+		if(Time.timeScale==0 && (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) ) {
+			Time.timeScale = 1;
+			startText.SetActive(false);
+			//GetComponent<SpriteRenderer>().enabled = false;
+			sawOnce=false;
+		}
+	}
+}
